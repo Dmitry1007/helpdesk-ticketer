f43839ea9223d7edf51e1f784b0e358a46afd131
Dmitry Sky <dmitry.sky@icloud.com>
2015-12-06 14:47:47 -0700

    add socket.io

--- server/app.js
+++ server/app.js
@@ -1,4 +1,4 @@
-var express = require(express);
+var express = require('express');
 var socketio = require('socket.io');
 var app = express();
 
@@ -6,4 +6,10 @@ var io = socketio.listen(app.listen(3000));
 
 io.sockets.on('connection', function (socket) {
   console.log('A user has connected');
+
+  socket.on('ticket-submitted', function (ticket) {
+    // Inside this callback it may be a good idea to manipulate the data however you need, maybe persist it to a database, and so on.
+    console.log('A ticket has been submitted');
+    socket.broadcast.emit('ticket-submitted', ticket);
+  });
 });
